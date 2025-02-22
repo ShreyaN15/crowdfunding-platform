7e5213c27ebaec1749f970a4dfd4fb3e10ba0757
ShreyaN15 <nairshreya2016@gmail.com>
2025-02-22 19:03:38 +0530

    campaign images added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom"; // For navigation
 import { Carousel, Card, Button } from "react-bootstrap"; // Bootstrap Components
+import img1 from "../assets/camp.png";
+import img2 from "../assets/camp2.jpeg";
+
 
 const Home = () => {
     console.log("Home Component Loaded");
@@ -24,6 +27,7 @@ const Home = () => {
       <section className="text-center mt-5 p-4 bg-light">
         <h2>About Us</h2>
         <p>We empower projects through community support. Join us to fund and get funded.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptatibus itaque dicta adipisci beatae, quas, dolorem alias fuga cumque, iure numquam qui quisquam eum tempora laboriosam perspiciatis ex molestias accusantium.</p>
       </section>
 
       {/* Campaign Slider */}
@@ -31,13 +35,13 @@ const Home = () => {
         <h2 className="text-center">Trending Campaigns</h2>
         <Carousel>
           <Carousel.Item>
-            <img className="d-block w-100" src="https://via.placeholder.com/800x400" alt="Campaign 1" />
+            <img className="d-block w-100" src={img2} alt="Campaign 1" />
             <Carousel.Caption>
               <h3>Save the Environment</h3>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img className="d-block w-100" src="https://via.placeholder.com/800x400" alt="Campaign 2" />
+            <img className="d-block w-100" src={img1} alt="Campaign 2" />
             <Carousel.Caption>
               <h3>Help a Family in Need</h3>
             </Carousel.Caption>
